226b1bb8bb5b60055cfb681c9666c9057739f1d3
cremubi <allanfernandospereira@gmail.com>
2025-02-24 19:20:51 -0300

    entrega de desafio

--- classificador.js
+++ classificador.js
@@ -1,29 +1,30 @@
 const nomeHeroi = "Cremubi"
+const nivel = ""
 let xp = 4999
 
 if (xp < 1000){
-    var nivel = "Ferro"
+    nivel = "Ferro"
 }
 else if (xp >= 1000 && xp < 2000){
-    var nivel = "Bronze"
+    nivel = "Bronze"
 }
 else if (xp >= 2000 && xp < 5000){
-    var nivel = "Prata"
+    nivel = "Prata"
 }
 else if (xp >= 5000 && xp < 7000){
-    var nivel = "Ouro"
+    nivel = "Ouro"
 }
 else if (xp >= 7000 && xp < 8000){
-    var nivel = "Platina"
+    nivel = "Platina"
 }
 else if (xp >= 8000 && xp < 9000){
-    var nivel = "Ascendente"
+    nivel = "Ascendente"
 }
 else if (xp >= 9000 && xp < 10000){
-    var nivel = "Imortal"
+    nivel = "Imortal"
 }
 else {
-    var nivel = "Radiante"
+    nivel = "Radiante"
 }
 
 console.log("O herói chamado " + nomeHeroi + " está no nível " + nivel + " com " + xp + " pontos de experiência.")
